d48623cbb2d23a24021e51b73b7bc1422e61f543
Anthony White <anhwhite@ucsc.edu>
2023-05-21 15:33:51 -0700

    Social Distance Detector Project 2022

---
+++ Social_Distance_Detector/Overall System Design/Main_System/BLE_System.ino
@@ -0,0 +1,13 @@
+void setupBLESystem(){
+  //setup code for pins, etc.
+  
+}
+
+int getBatteryLife(){
+  //returns battery life of device
+  return 10;
+  
+}
+
+
+//Send distance parameter
---
+++ Social_Distance_Detector/README.md
@@ -0,0 +1,5 @@
+# 123A
+123A  repo for social distancers
+By: Connor Stewart, Calvin Stickler
+ test2
+ 
